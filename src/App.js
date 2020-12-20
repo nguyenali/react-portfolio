49e860bbba06228e20d03841fd7ed9b342daccdb
Ali Nguyen <alin57717@gmail.com>
2020-12-19 21:09:18 -0800

    update page template to display components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 import React, { useState } from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
 import Gallery from './components/Gallery';
-import ContactForm from './components/Contact';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+    const [categories] = useState([
+        { name: 'About' },
+        { name: 'Portfolio' },
+        { name: 'Contact' },
+        { name: 'Resume' }
+    ]);
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-  const [contactSelected, setContactSelected] = useState(false);
+    return (
+        <div>
+            <Header>
+                <Navigation id="header-title"
+                    categories={categories}
+                    setCurrentCategory={setCurrentCategory}
+                    currentCategory={currentCategory}
+                ></Navigation>
+            </Header>
 
-  return (
-    <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-            <Gallery currentCategory={currentCategory}></Gallery>
-            <About></About>
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        )}
-      </main>
-      <Footer></Footer>
-    </div>
-  );
+            <main>
+                <Gallery currentCategory={currentCategory} />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,57 +1,15 @@
-import React, {useState} from 'react';
-import Project from '../Project';
+import React from 'react';
+import PhotoList from '../Header';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-const PhotoList = ({currentCategory}) => {
-
-  const [photos] = useState([
-    {
-      name: 'projects',
-      description: 'Run Buddy',
-      link: "https://nguyenali.github.io/run-buddy/",
-      github:"https://github.com/nguyenali/run-buddy",
-      id: 0
-    },
-    { name: 'projects', description: 'Work-Day-Planner',
-      link: "https://nguyenali.github.io/Work-Day-Scheduler/",
-      github: "https://github.com/nguyenali/Work-Day-Scheduler",
-       id: 1},
-    { name: 'projects', description: 'Weather Dashboard, Web API', 
-      link:"https://stormy-chamber-41832.herokuapp.com/",
-      github: "https://github.com/nguyenali/budget-accounter",
-      id: 2 },
-    { name: 'projects', description: 'Tech This Out - Tech blog, MVC', id: 3 },
-    {
-      name: 'projects',
-      description: 'SmollTalk - Community Forum, ORM & MVC',
-      id: 4
-    },
-    { name: 'projects', description: 'Budge-It - Budget tracker, PWA', id: 5 },
-    { name: 'photography', description: 'Nature encounters while camping in Bodega Bay - 35mm', id: 0 },
-    {
-      name: 'photography',
-      description: 'Lovers in 35mm', id: 1
-    },
-    { name: 'photography', description: 'Bustling streets of Tokyo at night', id:2 },
-    { name: 'photography', description: 'Tokyo traffic at night', id: 3 },
-    { name: 'photography', description: 'Daytime Tokyo traffic', id: 4 },
-    { name: 'photography', description: 'Coastal getaway', id: 5}
-  ]);
-
-    //selects the photos matching the category selected
-    const currentPhotos = photos.filter((photo) => photo.name === currentCategory);
-    console.log(currentCategory);
-    //renders each photo from the category
-    return (
-    <div>
-        {currentPhotos.map((image) => (
-          <Project name={image.name}
-            description={image.description}
-            link={image.link}
-            github={image.github}
-            id={image.id}/>
-        ))}
-    </div>
-    )
+function Gallery(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
 }
-
-export default PhotoList;
+export default Gallery;
