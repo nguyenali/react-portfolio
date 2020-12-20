53026ebeb88d54606b4d318cb808e94d01fc1f6a
Ali Nguyen <alin57717@gmail.com>
2020-12-19 21:14:47 -0800

    update nav component to display categories

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,57 +1,32 @@
 import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    contactSelected,
-    currentCategory,
-    setContactSelected,
-  } = props;
+function Navigation(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
-  return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"></span> My Portfolio
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
-            </a>
-          </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => setContactSelected(true)}>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <nav>
+            <ul id="header-nav" className="flex-row">
+                {categories.map(category => (
+                    <li className={`mx-1 ${currentCategory.name === category.name && `navActive`}`}
+                    key={category.name}
+                    >
+                        <a onClick={() => {setCurrentCategory(category)}} href={'#' + category.name}>
+                            {category.name}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 }
 
-export default Nav;
+export default Navigation;
