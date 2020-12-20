abe7a211664540f3435221da225b9bcbe42ab443
Ali Nguyen <alin57717@gmail.com>
2020-12-19 21:22:19 -0800

    update about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import defaultPhoto from '../../assets/images/defaultPhoto.JPG';
+
+
 function About() {
-  return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-            I am a bay area native that gradutated from UC DAVIS with a Bachelor's in Biological Sciences.
-            Although I intended to be on the pre-med route, the COVId-19 Pandemic hit and stalled my plans.
-            During that time, I realized the importance of technology and how much it was impacting the world.
-            I realized that, yes medicine does change lives, but technology can also change lives as well. And so
-            my journey into coding in hopes that someday I too can change the world. Contact me if interested in
-            my work.
-        </p>
-      </div>
-    </section>
-  );
+    return (
+        <div id="content">
+            <div class="container">
+                <div class="about">
+                    <div class="about-author">
+                        <img className="defaultPhoto" src={defaultPhoto} alt="Me at an event" ></img>
+                    </div>
+                    <h3 class="post-title">About Me</h3>
+                    <div class="post-content">
+                        <p>
+                        I am a bay area native that gradutated from UC DAVIS with a Bachelor's in Biological Sciences.
+                        Although I intended to be on the pre-med route, the COVId-19 Pandemic hit and stalled my plans.
+                        During that time, I realized the importance of technology and how much it was impacting the world.
+                        I realized that, yes medicine does change lives, but technology can also change lives as well. And so
+                        my journey into coding in hopes that someday I too can change the world. Contact me if interested in
+                        my work.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default About;
